8fc22bc4b2769fa8e56bb351435de6323d86e03b
KKXiaoKang <410080663@qq.com>
2025-07-21 16:34:15 +0800

    feat:正确的train config训练配置提交关于obs26 action10的agent维度

--- lerobot/config/Isaac_lab_kuavo_env/obs_image_dataset.py
+++ lerobot/config/Isaac_lab_kuavo_env/obs_image_dataset.py
@@ -217,7 +217,7 @@ def format_stats_for_config(stats_dict):
 
 if __name__ == "__main__":
     # 使用示例
-    repo_id = "KANGKKANG/rl_kuavo_718_1730"
+    repo_id = "KANGKKANG/rl_kuavo_721_1315"
     
     # 方法1：使用预计算的统计信息
     print("方法1：使用预计算的统计信息")
